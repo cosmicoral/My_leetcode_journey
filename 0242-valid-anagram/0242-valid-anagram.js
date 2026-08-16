b5b155aa4f3301a160c6c003d0ad86120958d39e
Coral_Han <sealoser5@gmail.com>
2026-08-16 23:18:43 +0100

    Time: 13 ms (65.24%), Space: 59.4 MB (25.82%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length){
+        return false;
+    }
+    const count = new Map();
+    for(const char of s){
+            count.set(char, (count.get(char)||0) +1);
+        }
+        for (const char of t){
+            if(!count.has(char) || count.get(char)===0){
+                return false;
+            }
+        count.set(char,count.get(char)-1)
+    };
+    return true;
+};
